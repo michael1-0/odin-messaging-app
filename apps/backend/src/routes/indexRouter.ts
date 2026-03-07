1fad1168b0dcfa51a8ca08c7a906f819eda0548e
Michael Bagaskoro Edwards <143170516+michael1-0@users.noreply.github.com>
2026-03-07 18:03:22 +0700

    Add initial shared user validation implementation

--- apps/backend/src/routes/indexRouter.ts
+++ apps/backend/src/routes/indexRouter.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import type { Router as RouterType } from "express";
 
-import { getHealth, postLogin, postSignup } from "../controllers/indexController.ts";
+import {
+  getHealth,
+  postLogin,
+  postSignup,
+} from "../controllers/indexController.ts";
+import validate from "express-zod-safe";
+import { UserSchema } from "@repo/zod-validations";
 
 const indexRouter: RouterType = Router();
 
 indexRouter.get("/health", getHealth);
-indexRouter.post("/sign-up", postSignup);
-indexRouter.post("/log-in", postLogin);
+indexRouter.post("/sign-up", validate({ body: UserSchema }), postSignup);
+indexRouter.post("/log-in", validate({ body: UserSchema }), postLogin);
 
 export default indexRouter;
--- packages/zod-validations/src/index.ts
+++ packages/zod-validations/src/index.ts
@@ -1,8 +1,20 @@
 import z from "zod";
 
-const User = z.object({
-  email: z.email(),
-  password: z.string(),
+const UserSchema = z.object({
+  email: z
+    .email("Email must be valid")
+    .min(3, "Email min length is 3 characters")
+    .max(255, "Email max length is 255 characters"),
+  password: z
+    .string("Password must be valid")
+    .min(2, "Password must be at least 2 characters long")
+    .max(20, "Password must not exceed 20 characters long")
+    .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .regex(/[0-9]/, "Password must contain at least one number"),
 });
+type User = z.infer<typeof UserSchema>;
 
-export { User };
+export default z;
+export { UserSchema };
+export type { User };
