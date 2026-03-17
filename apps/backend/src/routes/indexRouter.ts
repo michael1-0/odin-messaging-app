e1aab5f4c4fc61889349c1097b496bc90a2260fc
Michael Bagaskoro Edwards <143170516+michael1-0@users.noreply.github.com>
2026-03-17 10:01:10 +0700

    Add requireAuth in /health middleware chain

--- apps/backend/src/routes/indexRouter.ts
+++ apps/backend/src/routes/indexRouter.ts
@@ -8,10 +8,11 @@ import {
 } from "../controllers/indexController.ts";
 import validate from "express-zod-safe";
 import { UserSchema } from "@repo/zod-validations";
+import requireAuth from "../middlewares/auth.ts";
 
 const indexRouter: RouterType = Router();
 
-indexRouter.get("/health", getHealth);
+indexRouter.get("/health", requireAuth, getHealth);
 indexRouter.post("/sign-up", validate({ body: UserSchema }), postSignup);
 indexRouter.post("/log-in", validate({ body: UserSchema }), postLogin);
 
